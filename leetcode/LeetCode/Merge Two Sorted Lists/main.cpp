77d617290a0e9e15c581d8e5171ab312200a14ed
NorthWardTop <2394783277@qq.com>
2019-08-31 19:57:31 +0800

    mergeTwoLists2

--- leetcode/LeetCode/Merge Two Sorted Lists/main.cpp
+++ leetcode/LeetCode/Merge Two Sorted Lists/main.cpp
@@ -10,9 +10,9 @@ struct ListNode {
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
-        if (l1 == NULL)
+        if (l1 == NULL) //l1结束
             return l2;
-        else if (l2 == NULL) 
+        else if (l2 == NULL)  //l2结束, 返回l1
             return l1;
         else if (l1->val < l2->val) {
             l1->next = mergeTwoLists(l1->next, l2);
